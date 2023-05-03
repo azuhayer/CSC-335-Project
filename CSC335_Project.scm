6f2923010c366004905e8e5c0bd62cc39e8224e5
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-03 19:11:41 -0400

    Update CSC335_Project.scm

--- CSC335_Project.scm
+++ CSC335_Project.scm
@@ -2,3 +2,7 @@
 ; Final Project
 ; Group Members: Zuhayer Alvi and Anova Sarkar (Section M)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; GOAL: Develop and prove the correctness of various implementations 
+; of the graph data type (Adjacency List Representation and Adjacency Matrix Representation)
