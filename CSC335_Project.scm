c0694276b0873f584e2f44c5fb463fd57ca66d7f
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-03 19:28:35 -0400

    Update CSC335_Project.scm

--- CSC335_Project.scm
+++ CSC335_Project.scm
@@ -10,3 +10,7 @@
 ; DEFINITION OF GRAPH: A Graph 'G' is a non-linear data structure composed of a set of 'vertices' (V)
 ; and a set of 'edges' (E). The Graph G is denoted as G(V, E), V being the number of vertices and 
 ; E being the number of Edges.  
+
+; This information suggests that we will need data type for sets, vertices (V) and edges (E)
+
+
