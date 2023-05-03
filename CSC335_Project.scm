71b323ab7986aca25b74a5c4346e490616152a3e
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-03 19:57:52 -0400

    Update CSC335_Project.scm

--- CSC335_Project.scm
+++ CSC335_Project.scm
@@ -2,18 +2,22 @@
 ; Final Project
 ; Group Members: Zuhayer Alvi and Anova Sarkar (Section M)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; GOAL: Design and implement an Adjacency list representation of graphs and an Adjacency matrix representation
-; of graphs using adbstract data types. 
+; GOAL: Design and implement an Adjacency list representation of graphs and an 
+; Adjacency matrix representation of graphs using adbstract data types. 
 
 ; DEFINITION OF GRAPH: A Graph 'G' is a non-linear data structure composed of a set of 'vertices' (V)
 ; and a set of 'edges' (E). The Graph G is denoted as G(V, E), V being the number of vertices and 
 ; E being the number of Edges.  
 
-; This information suggests that we will also need data types for sets, vertices (V) and edges (E)
+; This information suggests that we may also need data types for sets, vertices (V) and edges (E)
 
 ; To help us in the process of creating data types for sets, vertices, edges and various 
 ; implementations of graphs, it would be beneficial to think about the various computations
 ; one would want to carry on such data types. 
+
+; Some suggestions for computations are:
+; Finding a path in G between vertices v1 and v2 
+; Finding a shortest path between vertices v1 and v2
 
