da6c7748aa2288466aaaf1d602dd67e8618a0e1e
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-10 13:37:51 -0400

    Update stack.scm

--- Alvi-Sarkar-Project/graph-package/basic-data-types/stack.scm
+++ Alvi-Sarkar-Project/graph-package/basic-data-types/stack.scm
@@ -3,3 +3,19 @@
 ; Group Members: Zuhayer Alvi and Anova Sarkar (Section M)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; ---- ABSTRACT ----
+; We know that a Stack is a linear data structure that follows the LAST IN FIRST OUT (LIFO) principle, 
+; meaning that the last element that is added to a Stack would be the first element that would be removed 
+; from the stack. 
+
+; The insertion of an element and the removal of an element within a Stack is defined through two main 
+; operations:
+
+; Push: Adds an element to the top of the stack
+; Pop: Removes and returns the topmost element of the stack
+
+; In the context of graphs, the Stack data type could be used in various search/traversal algorthms. 
+; One such application of the stack data type would be DFS or the Depth-First Search algorithm. 
+; The DFS algorithm does this by visiting a vertex of the graph and recursively visiting its 
+; adjacent vertices. 
