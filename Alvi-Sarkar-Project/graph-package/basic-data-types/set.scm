05cdf6b45a0984375446cbd5e2b305ed9344b5a4
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-13 13:46:31 -0400

    Update set.scm

--- Alvi-Sarkar-Project/graph-package/basic-data-types/set.scm
+++ Alvi-Sarkar-Project/graph-package/basic-data-types/set.scm
@@ -9,3 +9,16 @@
 ; In the context of graphs, a set data type could be useful for many graph calculations/information such as 
 ; finding a set of vertices or a set of edges. And because sets are able to store information about graphs 
 ; they could be used in different graph implementations such as Adjacency List or Adjacency Matrix. 
+
+; By utilizing the set data type in graphs, it would allow for graph algorithms to process and 
+; manage graph information, which would allow for various set operations for graphs. 
+
+
+; ----- CODE -----
+; In this implementation, we will represent a set as a list where each element appears only once. 
+; The 'make-set' function will create an empty list by using '().
+
+(define (make-set)
+    '())
+
+; 
