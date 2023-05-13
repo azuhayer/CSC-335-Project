6a3c740eee7e1f1128ab19b584f4c9348d4367f2
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-13 19:07:50 -0400

    Update set.scm

--- Alvi-Sarkar-Project/graph-package/basic-data-types/set.scm
+++ Alvi-Sarkar-Project/graph-package/basic-data-types/set.scm
@@ -16,4 +16,36 @@
 
 ; ----- CODE -----
 
+; WORK IN PROGRESS ;
+(define (empty-set)
+  '())
+
+(define (set-empty? set)
+  (null? set))
+
+(define (set-member? set element)
+  (memq element set))
+
+(define (set-add set element)
+  (if (set-member? set element)
+      set
+      (cons element set)))
+
+(define (set-remove set element)
+  (if (set-empty? set)
+      set
+      (if (equal? (car set) element)
+          (cdr set)
+          (cons (car set) (set-remove (cdr set) element)))))
+
+(define (set-union set1 set2)
+  (cond ((set-empty? set1) set2)
+        ((set-empty? set2) set1)
+        (else (set-union (set-add (cdr set1) (car set1)) set2))))
+
+(define (set-intersection set1 set2)
+  (cond ((set-empty? set1) '())
+        ((set-member? set2 (car set1))
+         (cons (car set1) (set-intersection (cdr set1) set2)))
+        (else (set-intersection (cdr set1) set2))))
 
