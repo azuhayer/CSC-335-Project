af3ff4716987ece6d4a32dc17c70ad26afbee233
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-13 13:27:52 -0400

    Update set.scm

--- Alvi-Sarkar-Project/graph-package/basic-data-types/set.scm
+++ Alvi-Sarkar-Project/graph-package/basic-data-types/set.scm
@@ -3,3 +3,9 @@
 ; Group Members: Zuhayer Alvi and Anova Sarkar (Section M)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; ----- ABSTRACT -----
+; A set is a data type that basically represents a collection of unique elements. 
+; In the context of graphs, a set data type could be useful for many graph calculations/information such as 
+; finding a set of vertices or a set of edges. And because sets are able to store information about graphs 
+; they could be used in different graph implementations such as Adjacency List or Adjacency Matrix. 
