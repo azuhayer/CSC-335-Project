ec5a842ac403a3c0307b6d4917b7b7986396fdb4
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-14 20:56:35 -0400

    Update graph.scm

--- Alvi-Sarkar-Project/graph-package/basic-data-types/graph.scm
+++ Alvi-Sarkar-Project/graph-package/basic-data-types/graph.scm
@@ -3,3 +3,15 @@
 ; Group Members: Zuhayer Alvi and Anova Sarkar (Section M)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; ----- ABSTRACT -----
+; A graph in computer programming represents a collection of interconnected nodes or commonly known as
+; vertices, along with edges that connect these vertices together. 
+
+; There are different types of graph data structures, some of which include: directed graphs
+; (where edges have a specific direction), undirected graphs (where edges have no direction), and lastly
+; weighted graphs (where edges have 'weights' or assigned values to them). 
+
+; Along with these different graph data structures, there are also various ways to implement a graph 
+; data type. Such implementations include are: adjacency list representation, and adjacency matrix 
+; representation.  
