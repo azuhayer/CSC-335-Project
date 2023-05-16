3e6508908c8cf68b9ef246b94962389bafb2da3d
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-15 23:00:14 -0400

    Update set-proof.scm

--- Alvi-Sarkar-Project/graph-package-docs/basic-data-types-docs/set-proof.scm
+++ Alvi-Sarkar-Project/graph-package-docs/basic-data-types-docs/set-proof.scm
@@ -3,3 +3,58 @@
 ; Group Members: Zuhayer Alvi and Anova Sarkar (Section M)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; ---------------------- ;
+;      'empty-set'       ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
+
+
+; ---------------------- ;
+;      'set-empty?'      ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
+
+
+; ---------------------- ;
+;      'set-member?'     ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
+
+
+; ---------------------- ;
+;      'set-add'         ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
+
+
+; ---------------------- ;
+;      'set-remove'      ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
+
+
+; ---------------------- ;
+;      'set-union'       ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
+
+
+; ---------------------- ;
+;   'set-intersection'   ;
+; ---------------------- ;
+; Precondition: 
+
+; Postcondition:
