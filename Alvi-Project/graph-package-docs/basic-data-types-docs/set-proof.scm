9b211901159b330f06f5a631ce1c740730a9ec66
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-19 12:36:38 -0400

    Update set-proof.scm

--- Alvi-Project/graph-package-docs/basic-data-types-docs/set-proof.scm
+++ Alvi-Project/graph-package-docs/basic-data-types-docs/set-proof.scm
@@ -25,9 +25,29 @@
 ; ---------------------- ;
 ;      'set-member?'     ;
 ; ---------------------- ;
-; Precondition: 
+; Precondition: The function 'set-member?' takes two parameters: a nonempty set and any 
+; element. 
 
-; Postcondition:
+; Postcondition: Will return true (#t) or false (#f) depending if the element given 
+; appears within the set. 
+
+; Inductive Hypopthesis: Assuming that the 'set-member?' function works properly for a set 
+; of 'n' elements, we want to show that it works correctly for a set of 'n + 1' elements 
+; as well. 
+
+; Inductive Step: By assuming the correctness of the function for sets of size 'n', we can 
+; also demonstrate that this function works with sets of 'n + 1' size by following 3 cases:
+;   1. When the set is empty, the function will return false (#f)
+;   2. If the first element of the set , '(car set)', equals to the target element, the 
+;      function will return true (#t)
+;   3. Otherwise, the function will recursively call 'set-member?' on the rest of the set
+
+; Base Case: When the set is empty, the function will return false (#f), thus the base case
+; holds. 
+
+; Since the base case holds and the inductive step shows that if these cases hold for a set of 
+; size 'n', then it must hold for a set of size 'n + 1', and thus we could conclude that the 
+; 'set-member?' function will correctly determine membership of all elements within a set. 
 
 
 ; ---------------------- ;
