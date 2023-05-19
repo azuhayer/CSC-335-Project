89c65e628a8e1bd0fca1c661ee6667209b884848
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-19 19:19:25 -0400

    updates

--- Alvi-Project/graph-package-docs/basic-data-types-docs/queue-proof.scm
+++ Alvi-Project/graph-package-docs/basic-data-types-docs/queue-proof.scm
@@ -7,19 +7,40 @@
 ; ---------------------- ;
 ;      'make-queue'      ;
 ; ---------------------- ;
+; Precondition: No explicit precondition needed to create a queue.
+
+; Postcondition: Creating a new queue will return an empty list: ().
+
 
 ; ---------------------- ;
 ;        'enqueue'       ;
 ; ---------------------- ;
+; Precondition: Function takes a queue and an element as parameters, queue doesnt necessarily
+; have to be empty. 
+
+; Postcondition: Will return a new queue with the element added to the rear of the 
+; queue. 
+
 
 ; ---------------------- ;
 ;        'dequeue'       ;
 ; ---------------------- ;
+; Precondition: Function takes a nonempty queue as a parameter. 
+
+; Postcondition: Will return a new queue with the frontmost element removed from the queue. 
+
 
 ; ---------------------- ;
 ;        'front'         ;
 ; ---------------------- ;
+; Precondition: 
+
+; Postcondition: 
+
 
 ; ---------------------- ;
 ;        'empty?'        ;
 ; ---------------------- ;
+; Precondition: 
+
+; Postcondition: 
