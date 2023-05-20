43f8c0ff4ce9d71f728e05a1298f3f76ac2924be
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-20 12:37:13 -0400

    Update dfs.scm

--- Alvi-Project/graph-package/graph-algorithms/dfs.scm
+++ Alvi-Project/graph-package/graph-algorithms/dfs.scm
@@ -4,9 +4,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Load stack.scm from basic-data-types folder
-; This is necessary since DFS will require the stack data type
+; Load stack.scm and graph.scm from basic-data-types folder
+; This is necessary since DFS will require the stack data type and the graph implementations
 (load "basic-data-types/stack.scm")
+(load "basic-data-types/graph.scm")
 
 ; ----- ABSTRACT -----
 ;
