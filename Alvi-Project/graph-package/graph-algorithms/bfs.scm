9fa609e09e19edd9fbea126143943577d64b67d1
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-20 12:34:40 -0400

    Update bfs.scm

--- Alvi-Project/graph-package/graph-algorithms/bfs.scm
+++ Alvi-Project/graph-package/graph-algorithms/bfs.scm
@@ -4,9 +4,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Load queue.scm from basic-data-types folder
-; This is necessary since BFS will require the queue data type
+; Load queue.scm and graph.scm from basic-data-types folder
+; This is necessary since BFS will require the queue data type and the graph implementations
 (load "basic-data-types/queue.scm")
+(load "basic-data-types/graph.scm")
 
 ; ----- ABSTRACT -----
 ; 
